58d3d11abb8fdf08cc5da973c90ad9a16e84ae8b
Narazaka <info@narazaka.net>
2019-05-29 20:18:36 +0900

    a

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 async function main() {
     // const mediaStream = await navigator.mediaDevices.getDisplayMedia({video: true, audio: true});
+    const $console = document.getElementById("c");
     const canvas = /** @type {HTMLCanvasElement} */(document.getElementById("c"));
     const ctx = canvas.getContext("2d");
     ctx.beginPath();
@@ -14,6 +15,9 @@ async function main() {
     const rec = new MediaRecorder(mediaStream);
     rec.ondataavailable = ({data}) => {
         console.log(data);
+        const line = document.createElement("div");
+        line.textContent = data.toString();
+        $console.appendChild(line);
     }
     rec.start(500);
 }
